11a0b49b2c5cb56b3e377ab74fc7611d09af462f
Nyle Melegrito <nyle.fbm@gmail.com>
2024-09-09 00:45:37 -0400

    updated deposit

--- bank-server/src/main/java/com/bankserver/commands/DepositCommand.java
+++ bank-server/src/main/java/com/bankserver/commands/DepositCommand.java
@@ -6,14 +6,16 @@ import java.util.HashMap;
 
 import com.bankserver.Bank;
 import com.bankserver.BankUser;
+import com.bankserver.enumerations.MessageHeaders;
+import com.bankserver.enumerations.ResponseStatusCodes;
 
 public class DepositCommand implements Command{
     private Bank bank = Bank.getBankInstance();
     private ObjectInputStream in;
     private ObjectOutputStream out;
-    private HashMap<String, String> request;
+    private HashMap<MessageHeaders, String> request;
 
-    public DepositCommand (ObjectInputStream in, ObjectOutputStream out, HashMap<String, String> request) {
+    public DepositCommand (ObjectInputStream in, ObjectOutputStream out, HashMap<MessageHeaders, String> request) {
         this.in = in;
         this.out = out;
         this.request = request;
@@ -22,25 +24,26 @@ public class DepositCommand implements Command{
     @Override
     public void execute() {
         try {
-            String cardNum = request.get("CARDNUM");
-            String pin = request.get("PIN");
-            double depositAmount = Double.parseDouble(request.get("DEPOSITAMOUNT"));
+            String cardNum = request.get(MessageHeaders.CARDNUM);
+            String pin = request.get(MessageHeaders.PIN);
+            double depositAmount = Double.parseDouble(request.get(MessageHeaders.DEPOSITAMOUNT));
+
+            HashMap<MessageHeaders, String> response = new HashMap<MessageHeaders, String>();
 
             BankUser user = bank.getBankUser(cardNum);
-            if(user == null) { //invalid card
+            if(user == null || !(user.authenticate(pin))) { //invalid card or pin
                 System.out.println("Request attempt with invalid credentials.\nCard number: " + cardNum + "\nPin: " + pin);
-                return;
-            }
-            if(!(user.authenticate(pin))) { //invalid pin
-                System.out.println("Request attempt with invalid credentials.\nCard number: " + cardNum + "\nPin: " + pin);
+                response.put(MessageHeaders.REQUESTTYPE, request.get(MessageHeaders.REQUESTTYPE));
+                response.put(MessageHeaders.RESPONSE_CODE, Integer.toString(ResponseStatusCodes.ERROR.code));
+                out.writeObject(response);
+                out.flush();
                 return;
             }
 
             user.deposit(depositAmount);
-            //create response
-            HashMap<String, String> response = new HashMap<String, String>();
-            response.put("REQUESTTYPE", request.get("REQUESTTYPE"));
-            response.put("RESPONSE", "Deposit successful.");
+            
+            response.put(MessageHeaders.REQUESTTYPE, request.get(MessageHeaders.REQUESTTYPE));
+            response.put(MessageHeaders.RESPONSE_CODE, Integer.toString(ResponseStatusCodes.SUCCESS.code));
             out.writeObject(response);
             out.flush();
 
--- bankatm/src/main/java/com/nyle/controllers/ControllerDepositPage.java
+++ bankatm/src/main/java/com/nyle/controllers/ControllerDepositPage.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 
 import com.nyle.ATM;
 import com.nyle.ATMModel;
+import com.nyle.enumerations.MessageHeaders;
+import com.nyle.enumerations.RequestTypes;
+import com.nyle.enumerations.ResponseStatusCodes;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -23,19 +26,19 @@ public class ControllerDepositPage extends Controller {
     @SuppressWarnings("unchecked")
     private boolean requestDeposit(String amount) {
         try {
-            HashMap<String, String> request = new HashMap<String, String>();
-            request.put("REQUESTTYPE", "DEPOSIT");
-            request.put("CARDNUM", model.getCardNum());
-            request.put("PIN", model.getPin());
-            request.put("DEPOSITAMOUNT", amount);
+            HashMap<MessageHeaders, String> request = new HashMap<MessageHeaders, String>();
+            request.put(MessageHeaders.REQUESTTYPE, RequestTypes.DEPOSIT.toString());
+            request.put(MessageHeaders.CARDNUM, model.getCardNum());
+            request.put(MessageHeaders.PIN, model.getPin());
+            request.put(MessageHeaders.DEPOSITAMOUNT, amount);
 
             out.writeObject(request);
             out.flush();
 
-            HashMap<String, String> response = (HashMap<String, String>) in.readObject();
-            System.out.println(response.get("RESPONSE"));
+            HashMap<MessageHeaders, String> response = (HashMap<MessageHeaders, String>) in.readObject();
+            System.out.println(response.get(MessageHeaders.RESPONSE_CODE));
 
-            return response.get("RESPONSE").equals("Deposit successful.");
+            return response.get(MessageHeaders.RESPONSE_CODE).equals(Integer.toString(ResponseStatusCodes.SUCCESS.code));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -60,11 +63,11 @@ public class ControllerDepositPage extends Controller {
                 System.out.println("Make another deposit?");
             }
             else{
-                System.out.println("Server error");;
+                System.out.println("Deposit failed");;
             }
         }
         else{
-            System.out.println("Error");
+            System.out.println("Input error");
         }
     }
 }
